22a6072b6ab27ff4cef2f59d8bf5b78736f685b1
Gajendra <raogajendra27@gmail.com>
2024-03-03 00:38:12 +0530

    added the functionality to the project

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-function addbtn(){
+function addbtn() {
     let title = document.getElementById('text').value;
     let description = document.getElementById('description').value;
 
-    let displaytext =  title  + "&nbsp; &nbsp; &nbsp; &nbsp;" + description;
+    if (title && description) {
 
+        let taskElement = document.createElement("div");
+        taskElement.className = "task";
+        taskElement.innerHTML ="<span>Title: " + title + "</span>" + "<br><span>Description: " + description + "</span>";
 
-    document.getElementById('display').innerHTML = displaytext;
+    
+       let delbtn = document.createElement('Button');
+       delbtn.className = "delete-button";
+       delbtn.innerText = "Delete";
+       delbtn.addEventListener('click', function(){
+               taskElement.remove()
+       })
+          
 
+        taskElement.appendChild(delbtn);
+
+        document.getElementById('display').appendChild(taskElement);
+
+        // console.log(`Title: ${title}, Description: ${description}`);
+
+        document.getElementById('text').value = '';
+        document.getElementById('description').value = '';
+    } else {
+        alert('Please fill out all fields');
+    }
 }
-
-
